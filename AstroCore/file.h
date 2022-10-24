74cec823a5c79c6996fd46fa5005f4f683bfd1c8
Ethan <epruhl102@gmail.com>
2022-10-24 18:40:26 -0400

    Cleanup

--- AstroCore/file.h
+++ AstroCore/file.h
@@ -1,4 +1,4 @@
-#include "../../include/raylib.h"
+#include <raylib.h>
 
 
 class File
--- AstroCore/input.h
+++ AstroCore/input.h
@@ -1,4 +1,4 @@
-#include "../../include/raylib.h"
+#include <raylib.h>
 #include <vector>
 #include <map>
 #include <string>
