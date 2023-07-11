ca77d870511a206bcf310633ca1978e7729b8b51
Ethan <epruhl102@gmail.com>
2023-07-10 22:30:36 -0400

    Physics testing pt3: The fall

--- AstroCore/src/physicsDebug.cpp
+++ AstroCore/src/physicsDebug.cpp
@@ -9,8 +9,8 @@ PhysicsDebug::PhysicsDebug()
 
 void PhysicsDebug::DrawPolygon(const b2Vec2 *vertices, int32 vertexCount, const b2Color &color)
 {
-    Vector2 *points = new Vector2[vertexCount ]();
-    points += vertexCount +1;
+    Vector2 *points = new Vector2[vertexCount]();
+    points += vertexCount + 1;
 
     if (vertexCount == 0)
     {
@@ -29,19 +29,19 @@ void PhysicsDebug::DrawPolygon(const b2Vec2 *vertices, int32 vertexCount, const 
     Vector2 point = *points;
     finalVert = b2Vec2(point.x, point.y);
 
-    DrawLineStrip(points, vertexCount, BLUE);
-    Vector2* finalPoints = new Vector2[vertexCount]();
-    
+    DrawLineStrip(points, vertexCount, (Color){color.r * 255.0f, color.g* 255.0f, color.b* 255.0f, color.a* 255.0f});
+    Vector2 *finalPoints = new Vector2[vertexCount]();
+
     // Close the polygon
     *(finalPoints) = {finalVert.x, finalVert.y};
     *(finalPoints + 1) = {firstVert.x, firstVert.y};
-    DrawLineStrip(finalPoints, 2, BLUE);
+    DrawLineStrip(finalPoints, 2, (Color){color.r* 255.0f, color.g* 255.0f, color.b* 255.0f, color.a* 255.0f});
 }
 
 void PhysicsDebug::DrawTransform(const b2Transform &xf)
 {
-    DrawCircleV({xf.p.x, xf.p.y}, 0.5f, GREEN);
+    DrawCircleV({xf.p.x, xf.p.y}, 0.5f, PURPLE);
 
     b2Vec2 vec = {cos(xf.q.GetAngle()) * 5, sin(xf.q.GetAngle()) * 5};
-    DrawLineV({xf.p.x, xf.p.y}, {xf.p.x + vec.x, xf.p.y + vec.y}, GREEN);
+    DrawLineV({xf.p.x, xf.p.y}, {xf.p.x + vec.x, xf.p.y + vec.y}, PURPLE);
 }
--- header/physicsScene.h
+++ header/physicsScene.h
@@ -13,22 +13,28 @@ public:
     {
         currentCamera = new CameraEntity();
         RegisterEntity(currentCamera);
-        currentCamera->zoom = 9.0;
-        Game::GetPhysicsWorld()->SetGravity({0,9.8});
+        // currentCamera->zoom = 9.0;
+        currentCamera->SetRenderDimensions(320, 180);
+        Game::GetPhysicsWorld()->SetGravity({0, 9.8});
 
         PhysicsEntity *floor = new PhysicsEntity(STATIC, {-2, 0});
-        floor->CreateRectangleCollider({0, 0}, {720, 20});
+        floor->CreateRectangleCollider({0, 0}, {720, 20},0,1.0,1.0,0.0);
         RegisterEntity(floor);
+        // PhysicsEntity *box1 = new PhysicsEntity(DYNAMIC, {-2, -50});
+        // box1->CreateRectangleCollider({0, 0}, {5, 5});
+        // RegisterEntity(box1);
 
-        PhysicsEntity *box1 = new PhysicsEntity(DYNAMIC, {-2, -50});
-        box1->CreateRectangleCollider({0, 0}, {5, 5});
-        RegisterEntity(box1);
+        // PhysicsEntity *box2 = new PhysicsEntity(DYNAMIC, {-5, -60});
+        // box2->CreateRectangleCollider({0, 0}, {5, 5});
+        // RegisterEntity(box2);
 
-        PhysicsEntity *box2 = new PhysicsEntity(DYNAMIC, {-7, -60});
-        box2->CreateRectangleCollider({0, 0}, {5, 5});
-        RegisterEntity(box2);
+        for (int i = 0; i < 10; i++)
+        {
+            PhysicsEntity *box = new PhysicsEntity(DYNAMIC, {-i * 2.0, -50 + (-i * 10)});
+            box->CreateRectangleCollider({0, 0}, {5, 6});
+            RegisterEntity(box);
+        }
     }
-
 };
 
 #endif // __PHYSICSSCENE_H__
