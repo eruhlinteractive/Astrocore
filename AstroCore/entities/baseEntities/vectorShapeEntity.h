d116396a27d19f09d8bf1e4646beb4d3d125fc32
Ethan <epruhl102@gmail.com>
2023-09-12 20:54:44 -0400

    Vector shape definition

--- AstroCore/entities/baseEntities/vectorShapeEntity.h
+++ AstroCore/entities/baseEntities/vectorShapeEntity.h
@@ -1,5 +1,24 @@
 #ifndef __VECTORSHAPEENTITY_H__
 #define __VECTORSHAPEENTITY_H__
+#include "entity.h"
 
-//namespace Astrocore
+namespace Astrocore
+{
+    class VectorShapeEntity : public Entity2D
+    {
+    public:
+        VectorShapeEntity();
+        int AddCircle();
+        int AddRoundedRectOutline();
+        int AddRoundedRect();
+        int AddRectOutline();
+        int AddRect();
+        int AddLine();
+
+    private:
+        JSON data;
+        int shapeCount = 0;
+
+    }
+};
 #endif // __VECTORSHAPEENTITY_H__
